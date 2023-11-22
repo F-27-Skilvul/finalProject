a6001d2ae03c7d9a3bddd9dd9dc0ce47997676ec
suryaprmdya <suryaoktova.p@gmail.com>
2023-11-23 00:46:40 +0700

    create component footer

--- FE-Ademyst/src/App.jsx
+++ FE-Ademyst/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
+import NotFound from './pages/notFound'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   )
