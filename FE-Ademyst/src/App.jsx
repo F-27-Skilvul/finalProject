85565c9f23321fec5c940093e9834061a61e3f74
Andiagung <ipossible24@gmail.com>
2023-11-23 08:48:51 +0800

    init detail bacaan

--- FE-Ademyst/src/App.jsx
+++ FE-Ademyst/src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import NotFound from './pages/notFound'
+import PreviewBacaan from './pages/previewBacaan'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='*' element={<NotFound />} />
+        {/* <Route path='/' element={<Home />} /> */}
+        <Route path='/' element={<PreviewBacaan />} />
+        {/* <Route path='*' element={<NotFound />} /> */}
       </Routes>
     </>
   )
---
+++ FE-Ademyst/src/components/detailBacaan/headerBacaan.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HeaderPreviewComponent = () => {
+  return (
+    <div className="container max-w-screen-xl pt-4 text-center">
+      <h2 className="text-4xl font-extrabold text-primary">Data Analyst</h2>
+      <p className="my-4 text-lg text-gray-500 ">
+        Temukan dasar-dasar analisis data dan menjadi seorang Data Analyst yang kompeten. Baca sumber daya ini untuk memahami alat, teknik, dan konsep dasar dalam analisis data.
+      </p>
+    </div>
+  );
+};
+
+export default HeaderPreviewComponent;
