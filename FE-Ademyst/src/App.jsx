314a78d8f544287ab075dfdf25ca13d8d34be1d2
suryaprmdya <suryaoktova.p@gmail.com>
2023-12-01 05:52:10 +0700

    fix route login & app provider

--- FE-Ademyst/src/App.jsx
+++ FE-Ademyst/src/App.jsx
@@ -3,12 +3,16 @@ import Home from './pages/home'
 import Pelajari from './pages/pelajari'
 import NotFound from './pages/notFound'
 import PreviewBacaan from './pages/previewBacaan'
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/pelajari' element={<Pelajari />} />
         <Route path='/pelajari/:title' element={<PreviewBacaan />} />
         <Route path='*' element={<NotFound />} />
