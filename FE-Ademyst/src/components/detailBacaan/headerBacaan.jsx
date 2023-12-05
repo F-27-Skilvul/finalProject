1b411ac718700ff5e3e6b81309d33e46f44874ce
suryaprmdya <suryaoktova.p@gmail.com>
2023-12-05 14:41:23 +0700

    dashboard-user

--- FE-Ademyst/src/components/detailBacaan/headerBacaan.jsx
+++ FE-Ademyst/src/components/detailBacaan/headerBacaan.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../../context/app-provider";
+import Message from "./message";
 
 function HeaderPreviewComponent() {
   const { bacaan, setBacaan } = useContext(AppContext);
@@ -11,15 +12,24 @@ function HeaderPreviewComponent() {
   );
 
   return (
-    <div className="container max-w-screen-xl pt-4 text-center items-center pb-4 flex flex-col md:flex-row md:text-start">
-      <img src={`https://ademystapi.adaptable.app/images/${pilihBacaan.image}`} className="max-w-xs justify-center" />
-      <div className="ml-4">
-        <h2 className="text-4xl font-extrabold text-primary">
-          {pilihBacaan.title}
-        </h2>
-        <p className="my-4 text-lg text-gray-500 ">{pilihBacaan.description}</p>
+    <>
+      
+      <div className="container max-w-screen-xl pt-4 text-center items-center pb-4 flex flex-col md:flex-row md:text-start">
+        <Message />
+        <img
+          src={`https://ademystapi.adaptable.app/images/${pilihBacaan.image}`}
+          className="max-w-xs justify-center"
+        />
+        <div className="ml-4">
+          <h2 className="text-4xl font-extrabold text-primary">
+            {pilihBacaan.title}
+          </h2>
+          <p className="my-4 text-lg text-gray-500 ">
+            {pilihBacaan.description}
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- FE-Ademyst/src/pages/Dashboard.jsx
+++ FE-Ademyst/src/pages/Dashboard.jsx
@@ -1,11 +1,138 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/navbar/navbar";
+import Footer from "../components/footer/footer";
+import axios from "axios";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Welcome to the Dashboard</h1>
-        </div>
+  const [bacaanFav, setBacaanFav] = useState([]);
+
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    getBacaanFav();
+  }, []);
+
+  const getBacaanFav = async () => {
+    const { data } = await axios.get(
+      "https://ademystapi.adaptable.app/followCourse",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
+    setBacaanFav(data.followedCourses); 
+    // console.log(bacaanFav[0])
+  };
+
+  console.log(bacaanFav)
+
+  return (
+    <>
+      <Navbar />
+      <div className="relative overflow-x-auto shadow-md pb-24 bg-secondary">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Activity
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Topic
+                  <a href="#">
+                    <svg
+                      className="w-3 h-3 ml-1.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Progress Done
+                  <a href="#">
+                    <svg
+                      className="w-3 h-3 ml-1.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Status
+                  <a href="#">
+                    <svg
+                      className="w-3 h-3 ml-1.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {bacaanFav.map((bacaan, index) => (
+              <tr
+                key={index}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {bacaan.Course.title}
+                  {/* html dasar */}
+                </th>
+                <td className="px-6 py-4">16</td>
+                <td className="px-6 py-4">20%</td>
+                <th scope="col" className="p-4">
+                  <div className="flex items-center">
+                    <input
+                      id="checkbox-all-search"
+                      type="checkbox"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label htmlFor="checkbox-all-search" className="sr-only">
+                      checkbox
+                    </label>
+                  </div>
+                </th>
+                <td className="px-6 py-4 text-right">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >
+                    Edit
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Dashboard;
